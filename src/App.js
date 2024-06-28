0f03d6c621ee682a8506302992bca206c388fb94
chaincode-labs <dev@chaincode-labs.org>
2024-06-28 09:42:34 +0800

    xx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import WebApp from '@twa-dev/sdk';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {WebApp.initData}
-          <img src={WebApp.initDataUnsafe.user.photo_url}/>
         </p>
         <a
           className="App-link"
